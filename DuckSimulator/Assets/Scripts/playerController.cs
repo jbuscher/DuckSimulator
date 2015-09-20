f162d3f47a07417173a89ebc67fa5d42f5738b3c
John Buscher <jbuscher@uw.edu>
2015-09-20 16:25:58 -0700

    Changed camera a bit, and made the ball jump

--- DuckSimulator/Assets/Scripts/playerController.cs
+++ DuckSimulator/Assets/Scripts/playerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class playerController : MonoBehaviour {
 
 	public float speed;
+	public float jumpForce;
 	private Rigidbody rb;
 
 	void Start(){
@@ -14,8 +15,18 @@ public class playerController : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
+		Vector3 pos = gameObject.transform.position;
+
+		if (Input.GetKeyDown(KeyCode.Space) && pos.y == 0.5) {
+			Jump();
+		}
+
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce (movement * speed);
+	}
+
+	void Jump() {
+		GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 	}
 }
