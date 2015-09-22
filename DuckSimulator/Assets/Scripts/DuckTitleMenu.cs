c0902dfe64ba803390eee152af0dcb9e4cbf7021
Max Schreiber <maxschreiber@aol.com>
2015-09-22 14:28:23 -0700

    Starting the Title Menu screen...DISCO DUCK

---
+++ DuckSimulator/Assets/Scripts/DuckTitleMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckTitleMenu : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    private Rigidbody rb;
+
+    Vector3 resetLoc;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        resetLoc = this.transform.position;
+        StartCoroutine(ResetDuck());
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = 0f;
+        float moveVertical = -1f;
+
+        Vector3 pos = gameObject.transform.position;
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        if (rb)
+            rb.AddForce(movement * speed);
+    }
+
+    IEnumerator ResetDuck()
+    {
+        this.transform.position = resetLoc;
+        yield return new WaitForSeconds(5);
+        StartCoroutine(ResetDuck());
+    }
+}
---
+++ DuckSimulator/Assets/Scripts/MainCameraTitleMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraTitleMenu : MonoBehaviour {
+
+    Vector3 pos1 = new Vector3(0f, 1f, -10f);
+    Vector3 pos2 = new Vector3(7.5f, 1f, -5f);
+    Vector3 pos3 = new Vector3(-7.5f, 5f, -5f);
+    Vector3 duckPos;
+
+    public Transform Duck;
+
+    // Use this for initialization
+    void Start () {
+        duckPos = Duck.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        StartCoroutine(MoveCamera());
+	}
+
+    IEnumerator MoveCamera()
+    {
+        this.transform.position = pos1;
+        this.transform.LookAt(Duck);
+        yield return new WaitForSeconds(5);
+
+        this.transform.position = pos2;
+        this.transform.LookAt(Duck);
+        yield return new WaitForSeconds(5);
+
+        this.transform.position = pos3;
+        this.transform.LookAt(Duck);
+        yield return new WaitForSeconds(5);
+
+        StartCoroutine(MoveCamera());
+    }
+}
