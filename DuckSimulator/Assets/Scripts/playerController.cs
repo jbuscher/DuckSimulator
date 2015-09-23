6b1b5e129dca33e50d0987b483d79d3aa34a1da4
John Buscher <jbuscher@uw.edu>
2015-09-23 15:01:21 -0700

    Added Rotation to the duck, increasing jump heights for each time the duck jumps, third person view for the duck, and auto correction for the duck if he finds himself upsidedown in the water. apologies for the god commit

--- DuckSimulator/Assets/Scripts/playerController.cs
+++ DuckSimulator/Assets/Scripts/playerController.cs
@@ -3,32 +3,71 @@ using System.Collections;
 
 public class playerController : MonoBehaviour {
 
-	public float speed;
+	public float moveSpeed;
+	public float rotateSpeed;
 	public float jumpForce;
+	public float correctionSpeed;
 	private Rigidbody rb;
+	private int jumpCount;
+	private bool onWater;
+	private float waterHeight;
 
 	void Start(){
 		rb = GetComponent<Rigidbody>();
+		jumpCount = 1;
+		waterHeight = GameObject.Find ("WaterProDaytime").transform.position.y;
 	}
 
 	void FixedUpdate(){
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		if (rb) {
+			float moveRotate = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical") * -1;
 
-		Vector3 pos = gameObject.transform.position;
+			float rotation = gameObject.transform.rotation.y * Mathf.PI;
 
-		if (Input.GetKeyDown(KeyCode.Space) && pos.y < 6.5) {
-			Jump();
+			Vector3 pos = gameObject.transform.position;
+			onWater = Mathf.Abs(pos.y - waterHeight) < 1; //Is the duck within 1 unit of the water height
+
+			if (Input.GetKeyDown(KeyCode.Space) && onWater) {
+				Jump();
+			}
+
+			float moveX = 0, moveZ = 0;
+			if(moveVertical != 0) {
+				moveX = moveVertical * Mathf.Sin (rotation);
+				moveZ = moveVertical * Mathf.Cos (rotation);
+				Vector3 movement = new Vector3 (moveX, 0.0f, moveZ);
+				rb.AddForce (movement * moveSpeed);
+			}
+
+			if (moveRotate != 0) {
+				rb.transform.Rotate(moveRotate * Vector3.up * rotateSpeed * Time.deltaTime);
+			}
+
+			if(onWater)
+				CorrectOrientation();
 		}
-
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
-		if(rb)
-			rb.AddForce (movement * speed);
 	}
 
 	void Jump() {
-		if(rb)
-			rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+		rb.AddForce (Vector3.up * jumpForce * (jumpCount), ForceMode.Impulse);
+		jumpCount++;
+	}
+
+	void CorrectOrientation() {
+		float rotateX = rb.transform.rotation.x;
+		float rotateZ = rb.transform.rotation.z;
+		bool shouldRotateX = rotateX > .01 || rotateX < -.01;
+		bool shouldRotateZ = rotateZ > .01 || rotateZ < -.01;
+
+		if (rotateX > 0 && shouldRotateX)
+			rb.transform.Rotate (Vector3.left * Mathf.Abs(rotateX) * correctionSpeed);
+		else if (rotateX < 0 && shouldRotateX)
+			rb.transform.Rotate (Vector3.right * Mathf.Abs(rotateX) * correctionSpeed);
+
+		if (rotateZ > 0 && shouldRotateZ)
+			rb.transform.Rotate (Vector3.back * Mathf.Abs(rotateZ) * correctionSpeed);
+		else if (rotateZ < 0 && shouldRotateZ)
+			rb.transform.Rotate (Vector3.forward * Mathf.Abs(rotateZ) * correctionSpeed);
 	}
 }
