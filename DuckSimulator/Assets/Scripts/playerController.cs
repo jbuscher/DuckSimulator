ff28d8e700c047e8b0d4aff91d702a2d5a28074b
John Buscher <jbuscher@uw.edu>
2015-09-24 18:20:26 -0700

    Added build, small tweaks to explosion

--- DuckSimulator/Assets/Scripts/playerController.cs
+++ DuckSimulator/Assets/Scripts/playerController.cs
@@ -103,7 +103,7 @@ public class playerController : MonoBehaviour {
 		Instantiate(explosion, transform.position, transform.rotation);
 		transform.localScale = Vector3.zero;
 		rb.velocity = Vector3.zero;
-		yield return new WaitForSeconds(2.0f);
+		yield return new WaitForSeconds(3.0f);
 		Application.LoadLevel ("Plane");
 	}
 
